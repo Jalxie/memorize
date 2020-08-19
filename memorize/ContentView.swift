35c4f27d8058c50ce806d7ad04a15aacd95f5a58
Jalxiey <jalseyswag@gmail.com>
2020-08-19 16:03:13 +1000

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Jalxiey Xie on 19/8/20.
+//  Copyright © 2020 Jalxiey Xie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
